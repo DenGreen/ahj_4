e22a9bf812d04b9feef286faeee06181e3f2d3db
Denis Malyshev <malyshev_denis@outlook.com>
2021-09-15 18:26:44 +0300

    refactoring

--- src/js/__tests__/checkingNumber.test.js
+++ src/js/__tests__/checkingNumber.test.js
@@ -1,14 +1,14 @@
 import CheckingNumber from '../checkingNumber';
 
 test.each([
-  [34, 0],
-  [6011, 1],
-  [3528, 2],
-  [5018, 3],
-  [51, 4],
-  [4, 5],
-  [4026, 6],
-  [2, 7],
+  ["34", 0],
+  ["6011", 1],
+  ["3528", 2],
+  ["5018", 3],
+  ["51", 4],
+  ["4", 5],
+  ["4026", 6],
+  ["2", 7],
 ])(('При аргументе %i, должно получится %i'), (a, expected) => {
   const response = new CheckingNumber();
   expect(response.checking(a)).toBe(expected);
